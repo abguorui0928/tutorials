de8c17b72e8ec106070fbc1f1d3d14bd4cef44e4
guor <abguorui0928@gmail.com>
2016-11-30 10:06:12 +0800

    1. 降低spring版本，以使用低版本jdk 2. 增加日志配置

--- spring-boot/src/test/java/spring/io/boot/TestCache.java
+++ spring-boot/src/test/java/spring/io/boot/TestCache.java
@@ -6,39 +6,39 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import spring.io.boot.model.Book;
 import spring.io.boot.repository.BookRepository;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Application.class })
 public class TestCache {
 
-    @Autowired
-    private BookRepository bookRepository;
+	@Autowired
+	private BookRepository bookRepository;
 
-    @Autowired
-    private CacheManager   cacheManager;
+	@Autowired
+	private CacheManager cacheManager;
 
-    @Test
-    public void test() throws InterruptedException {
-        Book book1 = bookRepository.find("book1");
-        Assert.assertNull(book1);
-        bookRepository.update(new Book("book2", "book2"));
+	@Test
+	public void test() throws InterruptedException {
+		Book book1 = bookRepository.find("book1");
+		Assert.assertNull(book1);
+		bookRepository.update(new Book("book2", "book2"));
 
-        Book book2 = bookRepository.find("book2");
-        Assert.assertEquals("book2", book2.getTitle());
+		Book book2 = bookRepository.find("book2");
+		Assert.assertEquals("book2", book2.getTitle());
 
-        bookRepository.delete("book2");
-        Book book3 = bookRepository.find("book2");
-        Assert.assertNull(book3);
-        List<Book> list = bookRepository.findList();
-        Assert.assertEquals(2, list.size());
+		bookRepository.delete("book2");
+		Book book3 = bookRepository.find("book2");
+		Assert.assertNull(book3);
+		List<Book> list = bookRepository.findList();
+		Assert.assertEquals(2, list.size());
 
-        Book book6 = bookRepository.find("book6");
-        Assert.assertNull(book6);
-    }
+		Book book6 = bookRepository.find("book6");
+		Assert.assertNull(book6);
+	}
 }
