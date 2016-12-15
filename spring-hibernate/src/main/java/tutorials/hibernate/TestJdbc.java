7bf488b58d0e334c0cff6796f381483f981bede3
guor <abguorui0928@gmail.com>
2016-12-15 11:51:05 +0800

    提交jdbc测试

---
+++ spring-hibernate/src/main/java/tutorials/hibernate/TestJdbc.java
@@ -0,0 +1,34 @@
+package tutorials.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestJdbc {
+
+	public static void main(String[] args) throws Exception {
+		String createTableSql = "CREATE TABLE IF NOT EXISTS `t_event` (`id` int NULL AUTO_INCREMENT, `name` varchar(255) NULL, PRIMARY KEY (`id`));";
+		String insertDataSql = "insert into `t_event` (`name`) values ('test')";
+
+		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "root", "root");
+		try {
+			Statement stmt = conn.createStatement();
+			try {
+				// 创建表如果不存在
+				stmt.executeUpdate(createTableSql);
+				// 执行插入，返回自增id
+				stmt.executeUpdate(insertDataSql, Statement.RETURN_GENERATED_KEYS);
+				ResultSet rs = stmt.getGeneratedKeys();
+				if (rs.next()) {
+					int key = rs.getInt(1);
+					System.out.println(key);
+				}
+			} finally {
+				stmt.close();
+			}
+		} finally {
+			conn.close();
+		}
+	}
+}
